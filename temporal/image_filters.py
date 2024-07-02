1f6f6aec5700c6739b9938beadfe3a72c2ff5b82
Dominus Iniquitatis <iniquitatis@users.noreply.github.com>
2024-07-02 20:16:52 +0300

    Fixed the sharpening filter producing black images Not sure why skimage doesn't support the negative axis notation here.

--- temporal/image_filters.py
+++ temporal/image_filters.py
@@ -294,7 +294,8 @@ class SharpeningFilter(ImageFilter):
     radius: float = FloatParam("Radius", minimum = 0.0, maximum = 5.0, step = 0.1, value = 0.0, ui_type = "slider")
 
     def process(self, npim: NumpyImage, parallel_index: int, project: Project, frame_index: int, seed: int) -> NumpyImage:
-        return skimage.filters.unsharp_mask(npim, self.radius, self.strength, channel_axis = -1)
+        # NOTE: `ndim - 1` is intentional, as there's probably a bug in skimage
+        return skimage.filters.unsharp_mask(npim, self.radius, self.strength, channel_axis = npim.ndim - 1)
 
 
 class SymmetryFilter(ImageFilter):
