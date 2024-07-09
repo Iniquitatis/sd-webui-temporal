4ba9c8c9e5adbccb33457e12fd346bd2d3f64a62
Dominus Iniquitatis <iniquitatis@users.noreply.github.com>
2024-07-09 18:34:19 +0300

    Improved the usability of the color editor It's subjective, of course, however I find the default color picker often a PITA to control, even if it does provide more, well... control. (Oh man, I'm gonna regret all those JS/CSS customizations when the migration to Gradio 4.x happens.)

--- temporal/ui/color_editor.py
+++ temporal/ui/color_editor.py
@@ -3,7 +3,7 @@ from typing import Iterator
 import gradio as gr
 
 from temporal.color import Color
-from temporal.ui import Callback, ReadData, UIThing, UpdateData, UpdateRequest, Widget
+from temporal.ui import Callback, CallbackInputs, CallbackOutputs, ReadData, UIThing, UpdateData, UpdateRequest, Widget
 from temporal.ui.gradio_widget import GradioWidget
 
 
@@ -12,39 +12,66 @@ class ColorEditor(Widget):
         self,
         label: str = "",
         channels: int = 4,
+        step: float = 0.01,
         value: Color = Color(),
     ) -> None:
         super().__init__()
 
-        with GradioWidget(gr.Row):
-            self._color = GradioWidget(gr.ColorPicker, label = self._format_label(label, "RGB"), value = value.to_hex(3))
-            self._alpha = GradioWidget(gr.Slider, label = self._format_label(label, "Alpha"), minimum = 0.0, maximum = 1.0, step = 0.01, value = value.a, visible = channels == 4)
+        with GradioWidget(gr.Group):
+            GradioWidget(gr.HTML, f"<span class='temporal-composite-widget-title'>{label}</span>")
+
+            with GradioWidget(gr.Row):
+                with GradioWidget(gr.Column):
+                    self._r = GradioWidget(gr.Slider, label = self._format_label("R"), minimum = 0.0, maximum = 1.0, step = step, value = value.r, visible = channels >= 1, elem_classes = ["temporal-color-editor-slider"])
+                    self._g = GradioWidget(gr.Slider, label = self._format_label("G"), minimum = 0.0, maximum = 1.0, step = step, value = value.g, visible = channels >= 2, elem_classes = ["temporal-color-editor-slider"])
+                    self._b = GradioWidget(gr.Slider, label = self._format_label("B"), minimum = 0.0, maximum = 1.0, step = step, value = value.b, visible = channels >= 3, elem_classes = ["temporal-color-editor-slider"])
+                    self._a = GradioWidget(gr.Slider, label = self._format_label("A"), minimum = 0.0, maximum = 1.0, step = step, value = value.a, visible = channels >= 4, elem_classes = ["temporal-color-editor-slider"])
+
+                self._preview = GradioWidget(gr.ColorPicker, label = self._format_label("Preview"), interactive = False, value = value.to_hex(3), elem_classes = ["temporal-color-editor-picker"])
+
+        @self.callback("change", [self], [self._preview])
+        def _(inputs: CallbackInputs) -> CallbackOutputs:
+            return {self._preview: {"value": inputs[self].to_hex(3)}}
 
     @property
     def dependencies(self) -> Iterator[UIThing]:
-        yield self._color
-        yield self._alpha
+        yield self._r
+        yield self._g
+        yield self._b
+        yield self._a
 
     def read(self, data: ReadData) -> Color:
-        result = Color.from_hex(data[self._color])
-        result.a = data[self._alpha]
-        return result
+        return Color(
+            float(data[self._r]),
+            float(data[self._g]),
+            float(data[self._b]),
+            float(data[self._a]),
+        )
 
     def update(self, data: UpdateData) -> UpdateRequest:
         result: UpdateRequest = {
-            self._color: {},
-            self._alpha: {},
+            self._r: {},
+            self._g: {},
+            self._b: {},
+            self._a: {},
         }
 
         if isinstance(channels := data.get("channels", None), int):
-            result[self._alpha]["visible"] = channels == 4
+            result[self._r]["visible"] = channels >= 1
+            result[self._g]["visible"] = channels >= 2
+            result[self._b]["visible"] = channels >= 3
+            result[self._a]["visible"] = channels >= 4
 
         if isinstance(value := data.get("value", None), Color):
-            result[self._color]["value"] = value.to_hex(3)
-            result[self._alpha]["value"] = value.a
+            result[self._r]["value"] = value.r
+            result[self._g]["value"] = value.g
+            result[self._b]["value"] = value.b
+            result[self._a]["value"] = value.a
 
         return result
 
     def setup_callback(self, callback: Callback) -> None:
-        self._color.setup_callback(callback)
-        self._alpha.setup_callback(callback)
+        self._r.setup_callback(callback)
+        self._g.setup_callback(callback)
+        self._b.setup_callback(callback)
+        self._a.setup_callback(callback)
