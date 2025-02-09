5ab5f2621fd555cd119b566c4454f64539e76d62
Dominus Iniquitatis <iniquitatis@users.noreply.github.com>
2025-02-09 08:04:08 +0300

    More stuff

--- ui/scripts/base/image_editor.js
+++ ui/scripts/base/image_editor.js
@@ -1,6 +1,68 @@
+import {ToolButton} from "../../scripts/base/tool_button.js";
 import {ValueEditor} from "../../scripts/base/value_editor.js";
 import {Signal} from "../../scripts/core/signal.js";
+import {Widget} from "../../scripts/core/widget.js";
 import {createElement} from "../../scripts/utils/dom.js";
+
+class ImageViewer extends Widget {
+    constructor() {
+        super();
+
+        this.viewedElement = null;
+
+        this.style.background = "hsla(0 0% 0% / 75%)";
+        this.style.display = "none";
+        this.style.height = "100%";
+        this.style.justifyItems = "center";
+        this.style.left = "0";
+        this.style.position = "fixed";
+        this.style.top = "0";
+        this.style.width = "100%";
+        this.style.zIndex = "1";
+        this.addEventListener("click", () => {
+            this.value = null;
+        });
+
+        this._img = createElement(this, "img", (e) => {
+            e.style.height = "100%";
+            e.style.objectFit = "contain";
+            e.style.position = "absolute";
+            e.style.width = "100%";
+        });
+
+        this._closeButton = createElement(this, "div", (e) => {
+            e.innerText = "\u{274c}";
+            e.style.alignContent = "center";
+            e.style.color = "white";
+            e.style.cursor = "pointer";
+            e.style.fontSize = "4rem";
+            e.style.fontWeight = "bold";
+            e.style.height = "4rem";
+            e.style.position = "absolute";
+            e.style.right = "0";
+            e.style.textAlign = "center";
+            e.style.top = "0";
+            e.style.width = "4rem";
+        });
+    }
+
+    set value(value) {
+        if (!value) {
+            this.viewedElement = null;
+        }
+
+        this.style.display = value ? null : "none";
+
+        this._img.src = value ?? undefined;
+    }
+}
+customElements.define("image-viewer", ImageViewer);
+
+let imageViewer = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+    imageViewer = createElement(document.body, ImageViewer);
+});
 
 export class ImageEditor extends ValueEditor {
     constructor() {
@@ -13,24 +75,48 @@ export class ImageEditor extends ValueEditor {
             e.style.border = "var(--thin-border)";
             e.style.borderRadius = "var(--corners)";
             e.style.minHeight = "10rem";
+            e.style.position = "relative";
             e.style.textAlign = "center";
             e.style.userSelect = "none";
             e.style.width = "100%";
 
             this._input = createElement(e, "input", (e) => {
                 e.type = "file";
+                e.accept = "image/*";
                 e.style.height = "100%";
                 e.style.width = "100%";
                 e.addEventListener("change", () => {
-                    this.onValueChange.fire(e.value);
+                    let reader = new FileReader();
+                    reader.addEventListener("load", () => {
+                        this.value = reader.result;
+
+                        e.value = null;
+                    });
+                    reader.readAsDataURL(e.files[0]);
                 });
             });
 
             this._img = createElement(e, "img", (e) => {
+                e.style.cursor = "pointer";
                 e.style.display = "none";
                 e.style.height = "100%";
                 e.style.maxWidth = "100%";
                 e.style.objectFit = "contain";
+                e.addEventListener("click", () => {
+                    imageViewer.value = e.src;
+                    imageViewer.viewedElement = this;
+                });
+            });
+
+            this._deleteButton = createElement(e, ToolButton, (e) => {
+                e.label = "\u{274c}";
+                e.style.display = "none";
+                e.style.position = "absolute";
+                e.style.right = "0";
+                e.style.top = "0";
+                e.onClick.connect(() => {
+                    this.value = null;
+                });
             });
         });
 
@@ -50,6 +136,12 @@ export class ImageEditor extends ValueEditor {
 
         this._img.src = value ?? null;
         this._img.style.display = value ? null : "none";
+
+        this._deleteButton.style.display = value ? null : "none";
+
+        if (imageViewer.viewedElement == this) {
+            imageViewer.value = value;
+        }
 
         this.onValueChange.fire(this.value);
     }
