8aee5503d01170cf7a4f2da83c5ffd0c18af396c
Dominus Iniquitatis <iniquitatis@users.noreply.github.com>
2025-03-07 10:18:24 +0300

    More stuff

---
+++ temporal/pipeline_modules/filtering/flipping.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from temporal.general_data import GeneralData
+from temporal.meta.configurable import ConfigurableParam as Param
+from temporal.pipeline_modules.filtering import ImageFilter
+from temporal.utils.image import NumpyImage
+
+
+class FlippingFilter(ImageFilter):
+    name = "Flipping"
+
+    horizontal: bool = Param("Horizontal", value = False)
+    vertical: bool = Param("Vertical", value = False)
+
+    def process(self, image: NumpyImage, general: GeneralData, frame_index: int, seed: int) -> NumpyImage:
+        image = image.copy()
+
+        if self.horizontal:
+            image = np.flip(image, axis = 1)
+
+        if self.vertical:
+            image = np.flip(image, axis = 0)
+
+        return image
