4b0f4c66f819a9ff237e008e84e052f879d60dd9
Dominus Iniquitatis <iniquitatis@users.noreply.github.com>
2025-10-07 04:25:06 +0300

    Fielded stuff

--- ui/scripts/animation_editor.js
+++ ui/scripts/animation_editor.js
@@ -1,6 +1,4 @@
 import {Button} from "/scripts/base/button.js";
-import {Checkbox} from "/scripts/base/checkbox.js";
-import {ColorPicker} from "/scripts/base/color_picker.js";
 import {Column} from "/scripts/base/column.js";
 import {Dropdown} from "/scripts/base/dropdown.js";
 import {Form} from "/scripts/base/form.js";
@@ -8,11 +6,11 @@ import {ChoiceListEditor, ListEditor} from "/scripts/base/list_editor.js";
 import {NumberBox} from "/scripts/base/number_box.js";
 import {ReorderableAccordion, ReorderableElement} from "/scripts/base/reorderable_list.js";
 import {Row} from "/scripts/base/row.js";
-import {TextBox} from "/scripts/base/text_box.js";
 import {ToolButton} from "/scripts/base/tool_button.js";
 import {FieldManager} from "/scripts/core/field_manager.js";
 import {Signal} from "/scripts/core/signal.js";
 import {deepCopy, mapValues} from "/scripts/utils/object.js";
+import {getFieldDefinition} from "/scripts/object_field.js";
 
 class KeyframeEditor extends ReorderableElement {
     constructor(schema) {
@@ -32,49 +30,13 @@ class KeyframeEditor extends ReorderableElement {
             this._manager.manage(e, "frame");
         });
 
-        if (schema.type == "bool") {
-            this.createChild(Checkbox, (e) => {
-                e.style.width = "100%";
-                e.value = schema.default ?? false;
-                this._manager.manage(e, "value");
-            });
-        } else if (schema.type == "int") {
-            this.createChild(NumberBox, (e) => {
-                e.style.width = "100%";
-                e.minimum = schema.minimum ?? undefined;
-                e.maximum = schema.maximum ?? undefined;
-                e.step = schema.step ?? 1;
-                e.value = schema.default ?? schema.minimum;
-                this._manager.manage(e, "value");
-            });
-        } else if (schema.type == "float") {
-            // FIXME: Treated as an integer by the backend when the value is
-            // rounded
-            this.createChild(NumberBox, (e) => {
-                e.style.width = "100%";
-                e.minimum = schema.minimum ?? undefined;
-                e.maximum = schema.maximum ?? undefined;
-                e.step = schema.step ?? 0.1;
-                e.value = schema.default ?? schema.minimum;
-                this._manager.manage(e, "value");
-            });
-        } else if (schema.type == "str") {
-            this.createChild(TextBox, (e) => {
-                e.style.width = "100%";
-                e.value = schema.default ?? "";
-                this._manager.manage(e, "value");
-            });
-        } else if (schema.type == "modules.color.Color") {
-            this.createChild(ColorPicker, (e) => {
-                e.style.width = "100%";
-                e.value = deepCopy(schema.default) ?? {r: 0.0, g: 0.0, b: 0.0, a: 1.0};
-                this._manager.manage(e, "value");
-            }, schema.channels ?? 3);
-        } else {
-            this.createChild("span", (e) => {
-                e.innerText = "Unhandled";
-            });
-        }
+        let {cls, initializer, args, reader, writer} = getFieldDefinition(schema);
+
+        this.createChild(cls, (e) => {
+            initializer(e);
+            e.style.width = "100%";
+            this._manager.manage(e, "value", reader, writer);
+        }, ...(args ?? []));
 
         this.createChild(ToolButton, (e) => {
             e.label = "\u{f0c5}";
